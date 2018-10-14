364d9b3e6fe5f649a7dc5fbb44b6f98e0380df8f
koalabzium <zosia.owsiak@gmail.com>
2018-10-14 23:09:34 +0200

    Added checking if no values are missing in read_file.

--- feedback_analysis/feedback_analysis.py
+++ feedback_analysis/feedback_analysis.py
@@ -15,6 +15,13 @@ def read_file(file_path):
         ec.is_shape(feedback, (-1, 6))
     else:
         raise ValueError("This file format is not supported yet.")
+
+    try:
+        ec.none_missing(feedback, ["message", "airline_code", "number_of_fellow_passengers",
+                                   "did_receive_compensation", "total_compensation_amount"])
+    except AssertionError:
+        print("Missing values")
+
     return feedback
 
 
@@ -24,28 +31,22 @@ def extract_messages(feedback):
 
 
 def plot_distribution_fellow_passengers(feedback):
-    feedback.plot(y='number_of_fellow_passengers', kind="hist")
+    feedback.plot(y="number_of_fellow_passengers", kind="hist")
     plt.pyplot.show()
 
 
 def calculate_average_compensation_per_passenger(feedback):
-    return (feedback["total_compensation_amount"] /
-            (feedback["number_of_fellow_passengers"] + 1)).mean()
+    fb_nonzero_compensation = feedback[feedback["total_compensation_amount"] != 0]
+    return (fb_nonzero_compensation["total_compensation_amount"] /
+            (fb_nonzero_compensation["number_of_fellow_passengers"] + 1)).mean()
 
 
 def calculate_got_the_compensation_percentage(feedback):
-    return np.count_nonzero(feedback['did_receive_compensation']) \
-           / len(feedback['did_receive_compensation']) * 100
+    return np.count_nonzero(feedback["did_receive_compensation"]) \
+           / len(feedback["did_receive_compensation"]) * 100
 
 
 def calculate_most_popular_airline(feedback):
     most_popular = feedback["airline_code"].mode()
     return most_popular.tolist()
 
-
-data = read_file('feedback_analysis/data.json')
-data2 = read_file('feedback_analysis/data.csv')
-
-print(calculate_most_popular_airline(data))
-print(extract_message(data))
-print(calculate_most_popular_airline(data2))
