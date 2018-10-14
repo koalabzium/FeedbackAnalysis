879912c2a220dca8f7225e6b2684a48870bdf484
koalabzium <zosia.owsiak@gmail.com>
2018-10-14 14:16:29 +0200

    Renamed tests

--- tests/test_feedback_analysis.py
+++ tests/test_feedback_analysis.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 import pandas as pd
-import feedback_analysis as fa
+from feedback_analysis import feedback_analysis as fa
 
 
 class TestRead(unittest.TestCase):
