a193fc792b838ba36db32581c0c313fa103c3c8f
koalabzium <zosia.owsiak@gmail.com>
2018-10-14 14:43:03 +0200

    Added extraxting messages.

--- feedback_analysis/feedback_analysis.py
+++ feedback_analysis/feedback_analysis.py
@@ -7,16 +7,20 @@ import engarde.checks as ec
 
 @ed.has_dtypes(items=dict(number_of_fellow_passengers=int, did_receive_compensation=int))
 def read_file(file_path):
-
     if file_path.lower().endswith(".json"):
         feedback = pd.read_json(file_path)
-        ec.is_shape(feedback, (-1,5))
+        ec.is_shape(feedback, (-1, 5))
     elif file_path.lower().endswith(".csv"):
         feedback = pd.read_csv(file_path)
-        ec.is_shape(feedback, (-1,6))
+        ec.is_shape(feedback, (-1, 6))
     else:
         raise ValueError("This file format is not supported yet.")
     return feedback
+
+
+def extract_messages(feedback):
+    messages = feedback["message"]
+    return messages
 
 
 def plot_distribution_fellow_passengers(feedback):
@@ -38,10 +42,10 @@ def calculate_most_popular_airline(feedback):
     most_popular = feedback["airline_code"].mode()
     return most_popular.tolist()
 
-data = read_file('data.json')
-data2 = read_file('data.csv')
+
+data = read_file('feedback_analysis/data.json')
+data2 = read_file('feedback_analysis/data.csv')
 
 print(calculate_most_popular_airline(data))
-print(calculate_got_the_compensation_percentage(data2))
-
-
+print(extract_message(data))
+print(calculate_most_popular_airline(data2))
