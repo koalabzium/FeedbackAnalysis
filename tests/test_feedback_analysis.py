763fb52c7da8f9cdd500916a2cebf54409eed7e8
koalabzium <zosia.owsiak@gmail.com>
2018-10-14 14:41:57 +0200

    Added testing functions on csv and json file.

--- tests/test_feedback_analysis.py
+++ tests/test_feedback_analysis.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 import pandas as pd
 from feedback_analysis import feedback_analysis as fa
+import feedback_analysis.create_data as create_data
 
 
 class TestRead(unittest.TestCase):
@@ -11,7 +12,7 @@ class TestRead(unittest.TestCase):
             fa.read_file("data.pdf")
 
 
-class TestAnalyse(unittest.TestCase):
+class TestAnalysis(unittest.TestCase):
 
     def setUp(self):
         d = dict(message=["Nie", "ma", "wody", "na"],
@@ -27,13 +28,38 @@ class TestAnalyse(unittest.TestCase):
 
     def test_most_popular_airline(self):
         result = fa.calculate_most_popular_airline(self.data)
-        self.assertEqual([1,2], result)
+        self.assertEqual([1, 2], result)
 
     def test_percentage_got_the_compensation(self):
         result = fa.calculate_got_the_compensation_percentage(self.data)
         self.assertEqual(50, result)
 
 
+class TestSameJsonCsv(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        data = create_data.generate_random_data(10)
+        create_data.serialize_tocsv(data, 'feedback_analysis/data.csv')
+        create_data.serialize_tojson(data, 'feedback_analysis/data.json')
+        cls.data_json = fa.read_file('feedback_analysis/data.json')
+        cls.data_csv = fa.read_file('feedback_analysis/data.csv')
+
+    def test_same_average_compensation_per_passenger(self):
+        result_json = fa.calculate_average_compensation_per_passenger(self.data_json)
+        result_csv = fa.calculate_average_compensation_per_passenger(self.data_csv)
+        self.assertEqual(result_csv, result_json)
+
+    def test_same_most_popular_airline(self):
+        result_json = fa.calculate_most_popular_airline(self.data_json)
+        result_csv = fa.calculate_most_popular_airline(self.data_csv)
+        self.assertEqual(result_csv, result_json)
+
+    def test_same_percentage_got_the_compensation(self):
+        result_json = fa.calculate_got_the_compensation_percentage(self.data_json)
+        result_csv = fa.calculate_got_the_compensation_percentage(self.data_csv)
+        self.assertEqual(result_csv, result_json)
+
+
 if __name__ == '__main__':
     unittest.main()
-
